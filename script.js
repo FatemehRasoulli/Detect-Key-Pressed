6789fc2750acfbc3559a263fb8e52d9c2ce9aa77
Fatemeh <Elnaz.rasoulii2001@gmail.com>
2025-05-19 15:12:35 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const result = document.querySelector("#result");
 window.addEventListener("keydown",(e)=>{
     result.innerHTML=`
-    The key is pressed <span> ${e.key} </span> <span> key code :${e.keycode} </span>
+    The key is pressed <span> ${e.key} </span> <span> key code :${e.keyCode} </span>
     `;
 });
